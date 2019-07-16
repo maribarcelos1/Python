50d5a57934e1d4c38f991948d2f91458558c5872
Mariana Barcelos <marianabarcelos13s@hotmail.com>
2019-07-15 23:06:37 -0300

    Create API de busca de palavras.py

---
+++ API de busca de palavras.py
@@ -0,0 +1,13 @@
+import requests
+
+palavra = str(input("Digite uma palavra"))
+parameter = {"rel_rhy":str(palavra)} # passando o parametro , rel_rhy é uma palavra chave da api para buscar as palavras mais semelhantes
+
+#chama o site passando os parametros como filtro
+requests = requests.get('https://api.datamuse.com/words',parameter)
+
+json = requests.json()
+for i in json[0:3]:
+    print(i['word'])
+
+#fazer uma excessao de resposta
